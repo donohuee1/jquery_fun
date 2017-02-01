3e2e0e7984279334dcf1111758fae227aeb8acca
Elizabeth Donohue <elizabeth.r.donohue@gmail.com>
2017-02-01 12:07:26 -0800

    fixed javascript

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$( document ).ready(function()){//waiting for the document to be ready
+/*$( document ).ready(function()){//waiting for the document to be ready
 
 $('#container').on('click', function(){
   alert('You clicked the container div!')
@@ -48,4 +48,50 @@ $('#drinkInput').keydown(function(e){
     }
   }
 })
-}
+}*/
+
+$( document ).ready(function(){
+
+  var foods = ['burgers', 'fries', 'pasta', 'shrimp', 'sushi']
+
+  // for (var i = 0; i < foods.length; i++) {
+  //   $('#foods').append('<li>' + foods[i] + '</li>')
+  // }
+
+  foods.forEach(function(food){
+    $('#foods').append('<li>' + food + '</li>')
+  })
+
+  $('#drinks').append('<li>Mountain Dew</li><li>Sprite</li><li>Dr. Pepper</li>')
+
+  $('#addFood').on('click', function(){
+    var newFood = $('#foodInput').val()
+    if(newFood != '') {
+      $('#foods').append('<li>' + newFood + '</li>')
+      // $('<li />', {html: newFood}).appendTo('ul#foods')
+      $('#foodInput').val('')
+    }
+  })
+
+  $('#addDrink').on('click', function(){
+    var newDrink = $('#drinkInput').val()
+    if(newDrink != '') {
+      $('#drinks').append('<li>' + newDrink + '</li>')
+      $('#drinkInput').val('')
+    }
+  })
+
+  $('#drinkInput').keydown(function(e){
+    if(e.keyCode === 13){
+      var newDrink = $('#drinkInput').val()
+      if(newDrink !== '') {
+        $('#drinks').append('<li class="purpleText">' + newDrink + '</li>')
+        $('#drinkInput').val('')
+        $('#drinkInput').removeClass('purple')
+      }
+    } else {
+      $('#drinkInput').addClass('purple')
+    }
+  })
+
+})
